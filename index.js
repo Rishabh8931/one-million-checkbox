d2c0795fa6f0ef39dafac7bb108dbf34da9cf86e
Rishabh Pal <rishabh8931872407@gmail.com>
2026-07-17 20:05:12 +0530

    websocket implementation start

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import http from "node:http"
 import express from "express"
 import path from "path"
+import { Server } from "socket.io"
 
 
  const CHECKBOX_SIZE = process.env.CHECKBOX_SIZE || 100
@@ -14,6 +15,14 @@ async function main (){
           const server = http.createServer(app) // htttp server
           const PORT = process.env.PORT || 8080;
 
+          // seckets
+          const io = new Server()
+          io.attach(server)
+
+          io.on("connection", (socket) => {
+                    console.log(`socketId: ${socket.id} connected`)
+          })
+
 
           // express
           app.use(express.static(path.resolve("./public")))
@@ -21,7 +30,7 @@ async function main (){
           app.get('/checkboxes' , (req, res) => {
                     return res.json({checkboxes : state.checkboxes})
           })
-          
+
 
           app.get("/health", (req, res) => {
                     res.send("HELLO! helathy")
@@ -37,3 +46,6 @@ async function main (){
           
           
 }
+
+
+main()
